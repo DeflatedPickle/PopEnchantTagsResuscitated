3310de9fc16cfc3613dc3d2edec637a8ad2ae852
DeflatedPickle <deflatedpickle@gmail.com>
2022-01-01 22:51:44 +0000

    Moved from events to mixins

--- src/main/kotlin/com/deflatedpickle/popenchanttags/PopEnchantTagsResuscitated.kt
+++ src/main/kotlin/com/deflatedpickle/popenchanttags/PopEnchantTagsResuscitated.kt
@@ -1,28 +1,85 @@
-/* Copyright (c) 2021 DeflatedPickle under the MIT license */
+/* Copyright (c) 2021-2022 DeflatedPickle under the MIT license */
 
 package com.deflatedpickle.popenchanttags
 
-import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
+import net.fabricmc.api.ClientModInitializer
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.item.EnchantedBookItem
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtList
+import net.minecraft.text.LiteralText
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import java.awt.SystemColor.text
 
 @Suppress("UNUSED")
-object PopEnchantTagsResuscitated : ModInitializer {
+object PopEnchantTagsResuscitated : ClientModInitializer {
     private const val MOD_ID = "$[id]"
     private const val NAME = "$[name]"
     private const val GROUP = "$[group]"
     private const val AUTHOR = "$[author]"
     private const val VERSION = "$[version]"
 
-    var remainingHighlightTicks = -1f
-    var highlightingItemStack: ItemStack? = null
-    var ticks = 0L
+    val DISTANCE = 10
 
-    override fun onInitialize() {
+    override fun onInitializeClient() {
         println(listOf(MOD_ID, NAME, GROUP, AUTHOR, VERSION))
+    }
 
-        HudRenderCallback.EVENT.register(PopEnchantTagsRenderer)
-        ClientTickEvents.END_CLIENT_TICK.register(PopEnchantTagsTick)
+    fun moveItemTitle(matrixStack: MatrixStack, info: CallbackInfo) {
+        val mc = MinecraftClient.getInstance()
+        val currentStack = mc.player!!.mainHandStack
+
+        if (!currentStack.hasNbt()) return
+        if (MinecraftClient.getInstance().interactionManager?.hasStatusBars() == false) return
+
+        if (info.id == "move:head") {
+            matrixStack.push()
+            matrixStack.translate(0.0, -DISTANCE.toDouble(), 0.0)
+        } else matrixStack.pop()
+    }
+
+    fun drawExtraText(
+        matrices: MatrixStack?,
+        ci: CallbackInfo?,
+        mutableText: MutableText?,
+        width: Int,
+        x: Int,
+        y: Int,
+        opacity: Int
+    ) {
+        val mc = MinecraftClient.getInstance()
+        val textRenderer = mc.textRenderer
+        val currentStack = mc.player!!.mainHandStack
+
+        val tagList: NbtList = when (currentStack.item) {
+            is EnchantedBookItem -> EnchantedBookItem.getEnchantmentNbt(currentStack)
+            else -> currentStack.enchantments
+        }
+
+        val enchantmentList = mutableListOf<Text>()
+        ItemStack.appendEnchantments(enchantmentList, tagList)
+
+        val text = LiteralText("").apply {
+            for ((i, s) in enchantmentList.withIndex()) {
+                append(s)
+                if (i < enchantmentList.size - 1) {
+                    append(", ")
+                }
+            }
+        }.formatted(Formatting.ITALIC, Formatting.GRAY)
+
+        val textWidth = textRenderer.getWidth(text)
+
+        textRenderer.drawWithShadow(
+            matrices,
+            text,
+            (x + (width - textWidth) / 2).toFloat(),
+            y.toFloat() + DISTANCE,
+            0xFFFFFF + (opacity shl 24)
+        )
     }
 }
